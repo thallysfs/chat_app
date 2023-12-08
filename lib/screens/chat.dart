9da03fe5eae795606fc5b11de776a8e4dd0cddd0
thallysfs <thallys@hotmail.com>
2023-12-08 08:09:06 -0300

    feat: creating a screen to send message

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chatt_app/widgets/chat_messages.dart';
+import 'package:chatt_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,23 +9,25 @@ class Chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Chat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            icon: Icon(
-              Icons.exit_to_app,
-              color: Theme.of(context).colorScheme.primary,
+        appBar: AppBar(
+          title: const Text('Flutter Chat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              ),
             ),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in!'),
-      ),
-    );
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(child: ChatMessage()),
+            NewMessage(),
+          ],
+        ));
   }
 }
