bbe5ce748c1343ce102998a54bbaca7fe868abc2
thallysfs <thallys@hotmail.com>
2023-12-11 19:12:02 -0300

    feat: fineshed chat message style

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,4 +1,6 @@
+import 'package:chatt_app/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
@@ -6,6 +8,9 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // pegar usuário logado
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
+
     // Aqui usarei a estratégia do Stream para modificar o widget dependendo do valor
     // perceba que o Firestore possui suporte a essa funcionalidade, portanto será usado
     // no parâmetro estream
@@ -37,7 +42,7 @@ class ChatMessage extends StatelessWidget {
             child: Text('Algo deu errado!'),
           );
         }
-
+        // pegando dados da doc instanciada na linha 18
         final loadedMessages = chatSnapshots.data!.docs;
 
         return ListView.builder(
@@ -49,8 +54,32 @@ class ChatMessage extends StatelessWidget {
           // reverse joga o conteúdo para baixo ao invés do padrão que é no top
           reverse: true,
           itemCount: loadedMessages.length,
-          itemBuilder: (ctx, index) => {
+          itemBuilder: (ctx, index) {
             final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = index + 1 < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+
+            final currentMesageUserId = chatMessage['userId'];
+            final nextMessageUserId =
+                nextChatMessage != null ? nextChatMessage['userId'] : null;
+            final nextUserIsSame = nextMessageUserId == currentMesageUserId;
+
+            if (nextUserIsSame) {
+              // renderizar mensagens enviada pelo usuário logado
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMesageUserId,
+              );
+              // mensagens de outros usuários
+            } else {
+              return MessageBubble.first(
+                userImage: chatMessage['userImage0'],
+                username: chatMessage['username'],
+                message: chatMessage['text'],
+                isMe: nextMessageUserId == currentMesageUserId,
+              );
+            }
           },
         );
       },
